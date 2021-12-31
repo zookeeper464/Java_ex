26c8d2731a31216827551bb143ef726662194c48
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-12-31 21:46:48 +0900

    for구조와 byte,int의 차이점
    노션 참조

---
+++ For.java
@@ -0,0 +1,11 @@
+public class For {
+  public static void main(String[] args) {
+    int var1 = 125;
+    byte var2 = 125;
+    for (int i=0; i<5; i++){
+      var1++;
+      var2++;
+      System.out.println("var1 : "+var1+"\tvar2 : "+var2);
+    }
+  }
+}
