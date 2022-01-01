db1cc76463419cb0f6bde639a3191668a6ee0f57
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2022-01-02 00:33:35 +0900

    타입 설정
    노션 참조

--- Type.java
+++ Type.java
@@ -2,13 +2,14 @@ public class Type {
   public static void main(String[] args) {
     int var1 = 125;
     long var2 = 10000000000L;
-    char var3 = 65;
+    char var3 = 65; // 'A' 또는 '\u0041'
     float var4 = 3.14f;
     double var5 = 3.141591;
-    boolean isTest = 1 > 2;
+    boolean var6 = 1 > 2;
     System.out.println(var2);
     System.out.println(var3);
     System.out.println(var4);
     System.out.println(var5);
+    System.out.println(var6);
   }
 }
