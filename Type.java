29f88da821ce47b19690a47123e0a6a989222391
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2022-01-02 00:32:06 +0900

    타입 설정 방법
    노션 참조

--- Type.java
+++ Type.java
@@ -5,6 +5,7 @@ public class Type {
     char var3 = 65;
     float var4 = 3.14f;
     double var5 = 3.141591;
+    boolean isTest = 1 > 2;
     System.out.println(var2);
     System.out.println(var3);
     System.out.println(var4);
