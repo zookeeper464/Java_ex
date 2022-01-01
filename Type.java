2a68365df776114d253511c6b1d7edfb7ec028b6
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2022-01-01 23:55:45 +0900

    타입에 대한 종류
    노션 참조

---
+++ Type.java
@@ -0,0 +1,13 @@
+public class Type {
+  public static void main(String[] args) {
+    int var1 = 125;
+    long var2 = 10000000000L;
+    char var3 = 65;
+    float var4 = 3.14f;
+    double var5 = 3.141591;
+    System.out.println(var2);
+    System.out.println(var3);
+    System.out.println(var4);
+    System.out.println(var5);
+  }
+}
